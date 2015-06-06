e2dbfa262c6439a7ee117820be98cda65d961bfb
John Waldrep <johnwaldrep@gmail.com>
2015-06-06 06:20:24 -0400

    Clean up functions further and add tests; 4/5 passing

--- lang_classifier.py
+++ lang_classifier.py
@@ -11,27 +11,32 @@ import numpy as np
 import pickle
 import os.path
 
+# TODO: Future Ideas:
+# use n-grams?
+# inverse_transform or otherwise make an best-case exemplar
+# web scraping
 
 def make_extension_dict():
-    extensions = {'C' : ['gcc', 'c'],
-                  'C#' : 'csharp',
-                  'Common Lisp' : 'sbcl',
-                  'Clojure' : 'clojure',
-                  'Haskell' : 'haskell',
-                  'Java' : 'java',
-                  'JavaScript' : 'javascript',
-                  'OCaml' : 'ocaml',
-                  'Perl' : 'perl',
-                  'PHP' : ['hack','php'],
-                  'Python' : 'python3',
-                  'Ruby' : ['jruby', 'yarv'],
-                  'Scala' : 'scala',
-                  'Scheme' : 'racket',
+    extensions = {'C': ['gcc', 'c'],
+                  'C#': 'csharp',
+                  'Common Lisp': 'sbcl',
+                  'Clojure': 'clojure',
+                  'Haskell': 'haskell',
+                  'Java': 'java',
+                  'JavaScript': 'javascript',
+                  'OCaml': 'ocaml',
+                  'Perl': 'perl',
+                  'PHP': ['hack', 'php'],
+                  'Python': 'python3',
+                  'Ruby': ['jruby', 'yarv'],
+                  'Scala': 'scala',
+                  'Scheme': 'racket',
                   }
+
     ext_lookup = {}
     for key, value in extensions.items():
         """Flip the dictionary around"""
-        if type(value) == type([]): #hasattr(value, '__iter__'):
+        if type(value) == type([]):  # hasattr(value, '__iter__'):
             for value2 in value:
                 ext_lookup[value2] = key
         else:
@@ -46,8 +51,11 @@ def extract_extension(string):
 
 
 def load_bench_data(reload=False):
-    if os.path.isfile("bench.data") and not reload:
-        df = pickle.load( open( "bench.data", "rb" ) )
+    df = unpickle('bench.data', reload=reload)
+    # if os.path.isfile("bench.data") and not reload:
+    #     df = pickle.load( open( "bench.data", "rb" ) )
+    #     return df
+    if df is not None:
         return df
     df = pd.DataFrame(columns=['language', 'text'])
     files = glob.glob('bench/*/*')
@@ -61,7 +69,7 @@ def load_bench_data(reload=False):
                     df = df.append(data, ignore_index=True)
         except (IsADirectoryError, UnicodeDecodeError):
             pass
-    pickle.dump( df, open( "bench.data", "wb" ) )
+    pickle.dump(df, open("bench.data", "wb"))
     return df
 
 
@@ -70,25 +78,27 @@ def load_test_data():
     test_files = glob.glob('./test/*')
 
     for (idx, fn) in enumerate(test_files):
-    #     try:
+        #     try:
         with open(fn) as fh:
-    #         df.loc[extract_extension(fn)] = ''.join(fh.readlines())
-    #         data = {'language': extract_extension(fn),
-    #                 'text': ''.join(fh.readlines())}
-    #         if data['language'] and data['text']:
-    #             df = df.append(data, ignore_index = True)
-    #     except (IsADirectoryError, UnicodeDecodeError):
-    #         pass
-            #test_data['text'][idx] = ''.join(fh.readlines())
+            #         df.loc[extract_extension(fn)] = ''.join(fh.readlines())
+            #         data = {'language': extract_extension(fn),
+            #                 'text': ''.join(fh.readlines())}
+            #         if data['language'] and data['text']:
+            #             df = df.append(data, ignore_index = True)
+            #     except (IsADirectoryError, UnicodeDecodeError):
+            #         pass
+            # test_data['text'][idx] = ''.join(fh.readlines())
             test_data.ix[idx, 'text'] = ''.join(fh.readlines())
     return test_data
 
-def test_classifier(pipe, *split_args):
+
+def assess_classifier(pipe, *split_args):
     pipe.fit(split_args[0], split_args[2])
     train_score = pipe.score(split_args[0], split_args[2])
     test_score = pipe.score(split_args[1], split_args[3])
     print('Train score: {:.3f}, Test score: {:.3f}'.format(train_score, test_score))
     return pipe
+
 
 def longest_run_of_capitol_letters_feature(text):
     runs = sorted(re.findall(r"[A-Z]+", text), key=len)
@@ -97,24 +107,34 @@ def longest_run_of_capitol_letters_feature(text):
     longest = runs[-1]
     return [len(longest)]
 
+
 def percent_periods_feature(text):
     """Return percentage of text that is periods compared to total text length."""
     periods = text.count(".")
     return [periods / len(text)]
 
+
 def feature_vector(text):
     return longest_run_of_capitol_letters_feature(text) + percent_periods_feature(text)
+
+
+def unpickle(name, reload=False):
+    if os.path.isfile(name) and not reload:
+        df = pickle.load(open("bench.data", "rb"))
+        return df
+    else:
+        return None
 
 
 class CustomFeaturizer:
     def __init__(self, *featurizers):
         self.featurizers = featurizers
-        
+
     def fit(self, X, y=None):
         """All SciKit-Learn compatible transformers and classifiers have the
         same interface. `fit` always returns the same object."""
         return self
-        
+
     def transform(self, X):
         """Given a list of original data, return a list of feature vectors."""
         fvs = []
@@ -130,23 +150,24 @@ if __name__ == '__main__':
     y = df.language
     test_data = load_bench_data()
 
-    args = train_test_split(X, y, test_size=0.2, )#random_state=0) # X_train, X_test, y_train, y_test
+    args = train_test_split(X, y, test_size=0.2, )  # random_state=0) # X_train, X_test, y_train, y_test
 
     spam_pipe = Pipeline([('bag_of_words', CountVectorizer()),
                           ('bayes', MultinomialNB())])
-    classifier = test_classifier(spam_pipe, *args)
+    print(spam_pipe)
+    classifier = assess_classifier(spam_pipe, *args)
     classifier.predict(args[1].iloc[2])
 
     spam_pipe = Pipeline([('bag_of_words', CountVectorizer()),
-                       ('tfidf', TfidfTransformer()),
-                      ('RFC', RandomForestClassifier())])
+                          ('tfidf', TfidfTransformer()),
+                          ('RFC', RandomForestClassifier())])
     spam_pipe.set_params(RFC__n_estimators=1000)
-    classifier = test_classifier(spam_pipe, *args)
-
+    print(spam_pipe)
+    classifier = assess_classifier(spam_pipe, *args)
 
     test_data['guess'] = pd.DataFrame(spam_pipe.predict(test_data['text']))
     correct = test_data[test_data.language == test_data.guess]
-    print('Proportion of test data correctly labeled: {:.3f}'.format(len(correct)/len(test_data)))
+    print('Proportion of test data correctly labeled: {:.3f}'.format(len(correct) / len(test_data)))
 
     longest_run_of_capitol_letters_feature('ABCabddwAAAA absd AB sd A.AA.AAA')
     percent_periods_feature('. . . . ')
@@ -154,5 +175,3 @@ if __name__ == '__main__':
 
     featurizer = CustomFeaturizer(longest_run_of_capitol_letters_feature,
                                   percent_periods_feature)
-
-

cfff3c0b5a4a7fa04703b8a3a319a7841325995b
Alan R <alan.rominger@gmail.com>
2015-06-07 10:26:33 -0400

    polished workbook

--- lclassifier/lclassifier.py
+++ lclassifier/lclassifier.py
@@ -24,6 +24,7 @@ def acceptable_file(text):
     else:
         return False
 
+
 def clean_ext(textp):
     text = textp.strip()
     if text == "gcc" or text == "h" or text == "gpp":
@@ -51,6 +52,8 @@ def clean_ext(textp):
 
 llist = ["c", "cs", "sbcl", "clj", "hs", "java", "js",
          "ocaml", "pl", "php", "py", "ruby", "scala", "racket"]
+main_dir = "/Users/admin/Documents/week5/programming-language-classifier"
+
 
 def list_uniques(alist):
     rlist = []
@@ -59,21 +62,22 @@ def list_uniques(alist):
             rlist.append(item)
     return rlist
 
+
 def load_file_names():
     l = [0 for i in range(5)]
-    s = "../benchmarksgame-2014-08-31/benchmarksgame/bench/"
+    s = main_dir+"/benchmarksgame-2014-08-31/benchmarksgame/bench/"
     max_lvl = 4
     for i in range(max_lvl):
-        l[i] = glob(s+"*/"*i+"*.*")
+        l[i] = glob(s + "*/" * i + "*.*")
 #    l[0] = glob("benchmarksgame-2014-08-31/benchmarksgame/*/*/*/*/*.*")
 #    l2 = glob("benchmarksgame-2014-08-31/benchmarksgame/bench/*/*/*.*")
 #    filelist = l1 + l2
     filelist = []
     for i in range(max_lvl):
         filelist += l[i]
-    testlist = glob("../test/*")
+    testlist = glob(main_dir+"/test/*")
 
-    print("   total samples "+str(len(filelist)))
+    print("   total samples " + str(len(filelist)))
     return filelist, testlist
 
 
@@ -83,27 +87,27 @@ def load_files(filelist, testlist):
     ext_list = []
     for filename in filelist:
         i = filename.rfind(".")
-        ext = clean_ext(filename[i+1:])
+        ext = clean_ext(filename[i + 1:])
         if ext == "tcl":
             print(filename)
     #    print(ext, end=" - ")
     #    print(ext+ str(ext in ext_list) + " - "+str(ext_list))
-        if not ext in ext_list:
+        if ext not in ext_list:
             ext_list.append(ext)
         if acceptable_file(ext):
             ltype.append(ext)
             with open(filename, encoding="ISO-8859-1") as file:
-    #            print(filename)
+                #            print(filename)
                 contents.append(file.read())
 #    return contents, ltype
 
-    print(" number of usable files "+str(len(ltype)))
+    print(" number of usable files " + str(len(ltype)))
     print(" ")
-    print(" number of read file types:  "+str(len(ext_list)))
-    print(" number of recognized types: "+str(len(llist)))
+    print(" number of read file types:  " + str(len(ext_list)))
+    print(" number of recognized types: " + str(len(llist)))
     print(" summary of tile types")
     for ext in ext_list:
-        print(ext.ljust(12)+ "  ", end=" ")
+        print(ext.ljust(12) + "  ", end=" ")
         if ext in llist:
             print(ltype.count(ext), end=" ")
         print(" ")
@@ -115,32 +119,33 @@ def load_files(filelist, testlist):
 
     testcont = [0] * 32
     for filename in testlist:
-    #    print(filename)
+        #    print(filename)
         with open(filename) as file:
             di = filename.rfind("/")
-            i = int(filename[di+1:])
+            i = int(filename[di + 1:])
 #            print(filename+" "+str(i))
-            testcont[i-1] = file.read()
+            testcont[i - 1] = file.read()
     print(" ")
     return contents, ltype, testcont
-    #print(testlist)
+    # print(testlist)
+
 
 def read_answers():
-    with open("../test.csv") as csvfile:
+    with open(main_dir+"/test.csv") as csvfile:
         ans_list = csv.reader(csvfile, delimiter=",")
         ans = []
         print(ans_list)
         for row in ans_list:
             ans.append(clean_ext(row[1]))
-    print(" number of testing file types: "+str(len(list_uniques(ans))))
+    print(" number of testing file types: " + str(len(list_uniques(ans))))
 #            print(row[0])
     return ans
 
 
 def fit1(contents, ltype):
     pipe = Pipeline([('bag_of_words', CountVectorizer()),
-                          ('tfidf', TfidfTransformer()),
-                          ('bayes', MultinomialNB())])
+                     ('tfidf', TfidfTransformer()),
+                     ('bayes', MultinomialNB())])
     pipe.fit(contents, ltype)
     return pipe
 #    print(pipe.score(contents, ltype))
@@ -150,13 +155,14 @@ def fit1(contents, ltype):
 
 def fit2(contents, ltype):
     pipe = Pipeline([('bag_of_words', CountVectorizer()),
-#                          ('tfidf', TfidfTransformer()),
-                          ('bayes', MultinomialNB())])
+                     #                          ('tfidf', TfidfTransformer()),
+                     ('bayes', MultinomialNB())])
     pipe.fit(contents, ltype)
     return pipe
 #    print(pipe.score(contents, ltype))
 #    print(pipe.predict(testcont))
 #    return pipe.score(contents, ltype)
+
 
 def print_matrix(matrix, p_max=None):
     if p_max is None:
@@ -166,12 +172,13 @@ def print_matrix(matrix, p_max=None):
     for i in range(upper_limit):
         vector = matrix[i]
         for val in vector:
-            print(str(round(val, 3)).ljust(5)+",", end="")
+            print(str(round(val, 3)).ljust(5) + ",", end="")
         print("")
         #print([str(round(val, 3)) for val in vector])
 
 
 class CustomFeaturizer(TransformerMixin):
+
     def __init__(self):
         pass
         #self.featurizers = featurizers
@@ -213,24 +220,23 @@ class CustomFeaturizer(TransformerMixin):
         scl = ["^[ \t]*object \w*", "^[ \t]*(final)?val \w* ="]
         tcl = ["^[ \t]*proc \w*::\w* \{"]
         php = ["^[ \t]*(\w*)?( )?function \w*( )?\(&?\$\w*",
-                "^[ \t]*\$\w* ?=.*;$"]
+               "^[ \t]*\$\w* ?=.*;$"]
         ocaml = ["^[ \t]*let \w+", "^[ \t]*struct[ \t]*$"]
         perl = ["^[ \t]*my ", "^[ \t]*sub \w* \{"]
         gcc = ["^[ \t]*typedef \w* \w* ?\{", "^#include ?\<",
                "^using .*;$", "sealed"]
-#        reg_list = char_list + word_list
+
         reg_list = clojure + python + js + ruby + hs + clj + java + scl\
-                   + tcl + php + ocaml + perl + gcc + cish
-#        print(len(reg_list))
+            + tcl + php + ocaml + perl + gcc + cish
+
         matrix = []
         for text in X:
             v = [0] * len(reg_list)
             for i in range(len(reg_list)):
                 reg_expr = reg_list[i]
                 prog = re.compile(reg_expr, flags=re.MULTILINE)
-                val = len(prog.findall(text))#/len(text)
-                #if val > 0:
-                #    val = 1
+                val = len(prog.findall(text))  # /len(text)
+                # this was found to have best results over normalized forms
                 v[i] = val
             matrix.append(v)
         return matrix
@@ -258,6 +264,8 @@ def fit5(contents, ltype):
 
 
 def fit6(contents, ltype):
+    '''Random Forest uses multiple decision trees and selects the
+       tree out of all of those which has occurs the most'''
     custom_feature = CustomFeaturizer()
     pipe = make_pipeline(custom_feature, RandomForestClassifier())
     pipe.fit(contents, ltype)
@@ -278,10 +286,11 @@ def demo_class(X, y):
                 typecont[j] += text
     custom_feature = CustomFeaturizer()
     M = custom_feature.transform(typecont)
+    ratio = 1000 / max([max(vt) for vt in M])
     for j in range(len(M)):
-        print(types[j].ljust(8)+" ", end="")
+        print(types[j].ljust(8) + " ", end="")
         for k in range(len(M[0])):
-            print(str(int(M[j][k])).ljust(5), end="")
+            print(str(int(ratio*M[j][k])).ljust(5), end="")
         print("")
 
 
@@ -296,8 +305,8 @@ def default_action():
     print(" score for    training_set     test_set")
     for i in range(len(plist)):
         pipe = plist[i](X, y)
-        print(str(i).ljust(4)+" "+str(round(pipe.score(X, y),4)).ljust(8)\
-              +str(round(pipe.score(Xt, yt),4)).ljust(8))
+        print(str(i).ljust(4) + " " + str(round(pipe.score(X, y), 4)).ljust(8)
+              + str(round(pipe.score(Xt, yt), 4)).ljust(8))
     print(" ")
     for i in range(len(plist)):
         pipel[i] = plist[i](contents, ltype)
@@ -308,8 +317,8 @@ def default_action():
     A = pipe.predict(X)
     for i in range(len(A)):
         if A[i] != y[i]:
-#            print(" ")
-            print(y[i].ljust(6)+" misclassified as "+A[i])
+            #            print(" ")
+            print(y[i].ljust(6) + " misclassified as " + A[i])
             if y[i] in failed_to_classify:
                 failed_to_classify[y[i]] += 1
             else:
@@ -322,10 +331,10 @@ def default_action():
     print(" failure counts")
     print("  wrongly classified:")
     for ext in wrongly_classified:
-        print(ext.ljust(7) + "#"*wrongly_classified[ext])
+        print(ext.ljust(7) + "#" * wrongly_classified[ext])
     print("  failed to classify")
     for ext in failed_to_classify:
-        print(ext.ljust(7) + "#"*failed_to_classify[ext])
+        print(ext.ljust(7) + "#" * failed_to_classify[ext])
     print(" ")
 
     ans = read_answers()
@@ -334,8 +343,8 @@ def default_action():
     i = 0
     for pipe in pipel:
         i += 1
-        print(" score_quest "+str(i)+" "+str(pipe.score(testcont, ans)))
-        print(" pred "+str(i)+" "+str(pipe.predict(testcont)))
+        print(" score_quest " + str(i) + " " + str(pipe.score(testcont, ans)))
+        print(" pred " + str(i) + " " + str(pipe.predict(testcont)))
         print(" ")
 
     demo_class(testcont, ans)
@@ -346,17 +355,16 @@ if __name__ == "__main__":
         default_action()
     elif len(sys.argv) == 2:
         test_file = sys.argv[1]
-        print("Estimating file type of "+ test_file)
+        print("Estimating file type of " + test_file)
 
         filelist, testlist = load_file_names()
         X, y, testcont = load_files(filelist, testlist)
         pipe = fit6(X, y)
         with open(test_file) as f:
             test_contents = f.read()
-#        print(test_contents)
         est_ext = pipe.predict([test_contents])
 
-        print("Predicted extension: "+str(est_ext))
+        print("Predicted extension: " + str(est_ext))
 
     else:
         print("error: command line arguments not supported")
